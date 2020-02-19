72d94e9f343c9682bf0ded3ee834c353344c7ed7
keshav-c <keshav1985@gmail.com>
2020-02-19 16:16:23 +0530

    users show number of friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,5 +8,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.ordered_by_most_recent
+    @friends = @user.all_friends
   end
 end
