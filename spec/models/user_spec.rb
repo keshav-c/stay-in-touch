9438f1082369d5d71f2f61e48bca1ca8cd09e869
keshav-c <keshav1985@gmail.com>
2020-02-20 20:29:48 +0530

    DRY user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,34 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before  do
-    @user = User.new(name: "User name",
-                    email: "user@mail.com",
-                    password: "foobar")
-    @another_user = User.new(name: "Another guy",
-                             email: "user@mail.com",
-                             password: "barfoo")                
+  before do
+    @user = User.new(name: 'User name',
+                     email: 'user@mail.com',
+                     password: 'foobar')
+    @another_user = User.new(name: 'Another guy',
+                             email: 'user@mail.com',
+                             password: 'barfoo')
   end
 
-  it "is valid with a name, email and password" do
+  it 'is valid with a name, email and password' do
     expect(@user).to be_valid
   end
 
-  it "is invalid without name" do
-    @user.name =  nil
+  it 'is invalid without name' do
+    @user.name = nil
     @user.valid?
-    expect(@user.errors[:name]).to include("can't be blank")
-  end
-  
-  it "is invalid without an email address" do
-    @user.email = nil
-    @user.valid?
-    expect(@user.errors[:email]).to include("can't be blank")
+    expect(@user.errors[:name]).to include('can\'t be blank')
   end
 
-  it "is invalid with a duplicate email address" do
+  it 'is invalid without an email address' do
+    @user.email = nil
+    @user.valid?
+    expect(@user.errors[:email]).to include('can\'t be blank')
+  end
+
+  it 'is invalid with a duplicate email address' do
     @user.save
     @another_user.valid?
-    expect(@another_user.errors[:email]).to include("has already been taken")
+    expect(@another_user.errors[:email]).to include('has already been taken')
   end
 end
